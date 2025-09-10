356f96d10b0ee4540669092e28f6e9b8aef5417b
AldrinOid <rinmendoza2000@gmail.com>
2025-09-10 21:25:25 +0800

    add if-elif-else file

---
+++ If-Else/if-elif-else.py
@@ -0,0 +1,19 @@
+# using if-elif-else statement
+
+a = 10
+if a > 15:
+    print("a is greater than 15")
+elif a > 5:
+    print("a is greater than 5 but not greater than 15")
+else:
+    print("a is not greater than 5")
+
+
+b = 3
+if b > 15:
+    print("b is greater than 15")
+elif b > 5:
+    print("b is greater than 5 but not greater than 15")
+else:
+    print("b is not greater than 5")
+
