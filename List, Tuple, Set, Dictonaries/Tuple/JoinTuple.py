1fa39ac0d656774e119a9600c3a631678b821a9b
AldrinOid <rinmendoza2000@gmail.com>
2025-09-10 16:16:17 +0800

    add join tuple file

---
+++ List, Tuple, Set, Dictonaries/Tuple/JoinTuple.py
@@ -0,0 +1,13 @@
+# joining tuples
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+tuple4 = tuple2 + tuple1
+print(tuple4)
+
+# multiplying tuples
+fruits = ("apple", "banana", "cherry")  
+mytuple = fruits * 2
+print(mytuple)
