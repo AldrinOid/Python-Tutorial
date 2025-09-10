2fbd68021d980c94a5f9ad4b16021678cab186ce
AldrinOid <rinmendoza2000@gmail.com>
2025-09-10 21:23:53 +0800

    add if-else file

---
+++ If-Else/if-else.py
@@ -0,0 +1,14 @@
+# using if-else statement
+a = 10
+if a > 5:
+    print("a is greater than 5")
+else:
+    print("a is not greater than 5")
+
+
+b = 3
+if b > 5:
+    print("b is greater than 5")
+else:
+    print("b is not greater than 5")
+
