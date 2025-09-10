780c63b74f67bda4ade2165ac431fbb50c74dbd2
AldrinOid <rinmendoza2000@gmail.com>
2025-09-10 21:27:35 +0800

    add shorhand file

---
+++ If-Else/Shorthand.py
@@ -0,0 +1,14 @@
+# using shorthand if-else (ternary) operator
+a = 10
+print("a is greater than 5") if a > 5 else print("a is not greater than 5")
+
+b = 3
+print("b is greater than 5") if b > 5 else print("b is not greater than 5")
+
+# using shorthand if (single statement)
+c = 7
+print("c is greater than 5") if c > 5 else None
+
+# using if-elif-else shorthand
+d = 12
+print("d is greater than 15") if d > 15 else print("d is greater than 5 but not greater than 15") if d > 5 else print("d is not greater than 5")
