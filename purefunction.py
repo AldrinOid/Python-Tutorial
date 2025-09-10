d0ea6bfaba0dfc6b80ae91b6094c574d3016853a
AldrinOid <rinmendoza2000@gmail.com>
2025-09-10 22:06:16 +0800

    add some function in purefunction file

--- purefunction.py
+++ purefunction.py
@@ -10,3 +10,13 @@ print(my_list)
 print(new_list)
 
 
+def get_area_of_rectangle(length, width):
+    area = length * width
+    return area
+
+lenghtRectangle = float(input("Input here the lenth of the rectangle: "))
+widthRectangle = float(input("Input here the width of the rectangle: "))
+
+print(f"The area of the rectangle is {get_area_of_rectangle(lenghtRectangle, widthRectangle)}. It's amazing!")
+
+
