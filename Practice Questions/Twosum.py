eca8374a9dafa12927db5078c9ba8bccd976b2b1
AldrinOid <rinmendoza2000@gmail.com>
2025-09-11 16:26:52 +0800

    change something. nothing important

--- Practice Questions/Twosum.py
+++ Practice Questions/Twosum.py
@@ -11,15 +11,3 @@ def two_sum(nums, target):
 two_sum([2,7,11,15], 9)
 print(two_sum([2,7,11,15], 9))
 
-
-
-# new_list = [1,2,3,4,5]
-
-# for x in range(len(new_list)):
-#     print(x)
-#     # new_map = x + (x+1)
-#     # if new_map == 9:
-#     #     print(f"This is the target sum {new_map} and the numbers are {x} and {x+1}. in the indexes {new_list.index(x)} and {new_list.index(x+1)}")
-#     #     break
-#     # else:
-#     #     x += 1
