13670f00888bbcfcc96c908a0622ef591358c8e7
AldrinOid <rinmendoza2000@gmail.com>
2025-09-18 18:02:39 +0800

    delete some code in this file

--- Modulepython/testmodule.py
+++ Modulepython/testmodule.py
@@ -7,13 +7,3 @@ from mymodule import person1
 
 print(person1['first_name'])
 print(person1.items())
-
-
-# use indent() to format the output
-# json.dumps(x, indent=4)
-
-# use separators to change the default separators
-# json.dumps(x, indent=4, separators=(". ", " = "))
-
-# use sort_keys to sort the result alphabetically by key
-# json.dumps(x, indent=4, sort_keys=True)
