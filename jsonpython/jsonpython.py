d779096184605c5fbaff0fa6e711a9a6af089080
AldrinOid <rinmendoza2000@gmail.com>
2025-09-18 17:59:27 +0800

    add json file and folder

---
+++ jsonpython/jsonpython.py
@@ -0,0 +1,26 @@
+# use this to convert Python objects into JSON strings
+import json
+# a Python object (dict):
+x = {
+  "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+# convert into JSON:
+y = json.dumps(x)
+# the result is a JSON string:
+print(y)
+
+# parse JSON back into a Python dictionary:
+z = json.loads(y)
+print(z["name"])
+
+
+# use indent() to format the output (only in dumps)
+# json.dumps(x, indent=4)
+
+# use separators to change the default separators (only in dumps)
+# json.dumps(x, indent=4, separators=(". ", " = "))
+
+# use sort_keys to sort the result alphabetically by key (only in dumps)
+# json.dumps(x, indent=4, sort_keys=True)
