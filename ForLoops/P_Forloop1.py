3ff8e7dee49bf2ddad57866cf55372b50a71a9df
AldrinOid <rinmendoza2000@gmail.com>
2025-09-25 20:36:52 +0800

    change some file name. add forloop file for practice

---
+++ ForLoops/P_Forloop1.py
@@ -0,0 +1,40 @@
+thislist = ["apple", "brocolli", "strawberry", "cabbage", "mango", "kiwi"]
+
+thislist_n = [11,22,33,44,55]
+
+# iterate the items in list
+for x in thislist:
+    print(x)
+
+print("\n")
+
+# use len()
+print(len(thislist_n))
+# count the indexes of the list
+
+name = "aldrin"
+print(len(name))
+# return how many characters in the value of the variable name
+
+print("\n")
+
+# use range(len())
+for y in range(len(thislist)):
+    print(y)
+
+print("\n")
+
+# range(start, end, step)
+for num in range(6):
+    print(num)
+
+print("\n")
+
+for nums in range(1,6):
+    print(nums) 
+# number 6 is not included
+# kapag end, di na sya ilalabas or the number before the specified end in range function
+
+print("\n")
+for numss in range(2, 20, 2):
+    print(numss)
